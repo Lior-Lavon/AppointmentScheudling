37dce87560fca7cd7d1923026ec608ebeb26a15f
Lior Lavon <liorlavon554@gmail.com>
2022-02-18 18:28:16 +0100

    Setting ConnectionString and Identity tables

---
+++ AppointmentScheudling/Models/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentScheudling.Models
+{
+    public class DatabaseContext : IdentityDbContext<IdentityUser>
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+    }
+}
